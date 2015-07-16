561b1cf54ea6dc782b918fa4943a3785788e6d4f
Jerimiah Milton <milton_jerimiah@bah.com>
2015-07-16 14:44:45 -0500

    Fixed a bug with the migration file not being generated due to an unset configuration value.

--- lib/comfy_gallery/engine.rb
+++ lib/comfy_gallery/engine.rb
@@ -8,10 +8,10 @@ module ComfyGallery
       if defined?(ComfortableMexicanSofa)
         # applying configuraion
         ComfyGallery.configure do |conf|
-          conf.admin_route_prefix = ComfortableMexicanSofa.config.admin_route_prefix
-          conf.upload_options     = ComfortableMexicanSofa.config.upload_file_options
-          conf.admin_controller   = 'CmsAdmin::BaseController'
-          conf.form_builder       = 'ComfortableMexicanSofa::FormBuilder'
+          conf.admin_route_prefix ||= ComfortableMexicanSofa.config.admin_route_prefix
+          conf.upload_options     ||= ComfortableMexicanSofa.config.upload_file_options
+          conf.admin_controller   ||= 'CmsAdmin::BaseController'
+          conf.form_builder       ||= 'ComfortableMexicanSofa::FormBuilder'
         end
         # applying nav elements
         ComfortableMexicanSofa::ViewHooks.add(:navigation, '/admin/gallery/navigation')
